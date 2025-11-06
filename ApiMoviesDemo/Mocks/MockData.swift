8d0757aca2b6d9f5c0550b2a7eaae7bd32bf2305
Marissa belle <marissabellebindeki@gmail.com>
2025-11-06 14:55:29 -0500

    interacting with the api url

--- ApiMoviesDemo/Mocks/MockData.swift
+++ ApiMoviesDemo/Mocks/MockData.swift
@@ -1,31 +1,20 @@
 struct MockData {
     static let movies: [Movie] = [
-        Movie(
-                   id: 1,
-                   title: "Inception",
-                   originalTitle: "Inception",
-                   overview: "A thief who steals corporate secrets through the use of dream-sharing technology.",
-                   releaseDate: "2010-07-16",
-                   backdropPath: "/s3TBrRGB1iav7gFOCNx3H31MoES.jpg",
-                   posterPath: "/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg",
-                   voteAverage: 8.3,
-                   voteCount: 32000,
-                   genreIds: [28, 878, 12]
-               ),
+            Movie(
+                id: 1,
+                title: "Interstellar",
+                originalTitle: "Interstellar",
+                overview: "Explorers travel through a wormhole to ensure humanity’s survival.",
+                releaseDate: "2014-11-07",
+                backdropPath: "/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
+                posterPath: "/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+                voteAverage: 8.6,
+                voteCount: 28000,
+                genreIds: [12, 18, 878]
+                   ),
+               
                Movie(
                    id: 2,
-                   title: "Interstellar",
-                   originalTitle: "Interstellar",
-                   overview: "Explorers travel through a wormhole to ensure humanity’s survival.",
-                   releaseDate: "2014-11-07",
-                   backdropPath: "/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
-                   posterPath: "/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
-                   voteAverage: 8.6,
-                   voteCount: 28000,
-                   genreIds: [12, 18, 878]
-               ),
-               Movie(
-                   id: 3,
                    title: "Avatar",
                    originalTitle: "Avatar",
                    overview: "A paraplegic Marine is sent to the moon Pandora on a mission.",
@@ -37,7 +26,7 @@ struct MockData {
                    genreIds: [28, 12, 878]
                ),
                Movie(
-                   id: 4,
+                   id: 3,
                    title: "The Dark Knight",
                    originalTitle: "The Dark Knight",
                    overview: "Batman faces the Joker, a criminal mastermind who plunges Gotham into chaos.",
@@ -49,7 +38,7 @@ struct MockData {
                    genreIds: [28, 80, 18]
                ),
                Movie(
-                   id: 5,
+                   id: 4,
                    title: "Joker",
                    originalTitle: "Joker",
                    overview: "A mentally troubled comedian embarks on a downward spiral.",
@@ -61,7 +50,7 @@ struct MockData {
                    genreIds: [80, 18, 53]
                ),
                Movie(
-                   id: 6,
+                   id: 5,
                    title: "Avengers: Endgame",
                    originalTitle: "Avengers: Endgame",
                    overview: "The Avengers assemble once more to reverse Thanos' actions.",
@@ -73,7 +62,7 @@ struct MockData {
                    genreIds: [28, 12, 878]
                ),
                Movie(
-                   id: 7,
+                   id: 6,
                    title: "Black Panther",
                    originalTitle: "Black Panther",
                    overview: "T’Challa returns home to claim his rightful place as king of Wakanda.",
@@ -85,7 +74,7 @@ struct MockData {
                    genreIds: [28, 12, 878]
                ),
                Movie(
-                   id: 8,
+                   id: 7,
                    title: "Frozen II",
                    originalTitle: "Frozen II",
                    overview: "Elsa and Anna embark on a journey to discover the source of Elsa's powers.",
@@ -96,20 +85,9 @@ struct MockData {
                    voteCount: 14000,
                    genreIds: [16, 12, 10751]
                ),
+       
                Movie(
-                   id: 9,
-                   title: "The Lion King",
-                   originalTitle: "The Lion King",
-                   overview: "Simba returns to reclaim his homeland after exile.",
-                   releaseDate: "1994-06-24",
-                   backdropPath: "/wXsQvli6tWqja51pYxXNG1LFIGV.jpg",
-                   posterPath: "/2bXbqYdudS2xL9bVj5lHPQZQqI0.jpg",
-                   voteAverage: 8.5,
-                   voteCount: 19000,
-                   genreIds: [16, 18, 10751]
-               ),
-               Movie(
-                   id: 10,
+                   id: 8,
                    title: "Shrek",
                    originalTitle: "Shrek",
                    overview: "An ogre embarks on a quest to rescue a princess.",
--- ApiMoviesDemo/Services/MovieService.swift
+++ ApiMoviesDemo/Services/MovieService.swift
@@ -1,8 +1,19 @@
-//
-//  MovieService.swift
-//  ApiMoviesDemo
-//
-//  Created by Marissa Belle Bindeki on 2025-11-06.
+import Foundation
 //
 
-import Foundation
+
+class MovieService{
+   private let baseUrl = "https://api.themoviedb.org/3"
+   private let apiKey = "5b7daa9d15680c08f8a6516739b91403"
+
+    func fetchMoivie() throws -> [Movie]{
+    let urlAsString = "\(baseUrl)/movie/popular?api_key=\(apiKey)&language=en-US&page=1"
+        print(urlAsString)
+        guard let url = URL(string: urlAsString) else {throw URLError(.badURL)
+        }
+        
+        
+        
+        return[]
+    }
+}
